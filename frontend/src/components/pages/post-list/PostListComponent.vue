bdccfdb8bc1b35c0c3d7a53720570780d715c436
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-01-01 16:00:33 -0300

    Add components and routes

---
+++ frontend/src/components/pages/post-list/PostListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Post List</h1>
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import navbar from '../../nav/NavBarComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuerouter from 'vue-router'
+import VueRouter from 'vue-router'
+
+import PostComponent from './components/pages/post/PostComponent'
+import PostListComponent from './components/pages/post-list/PostListComponent'
+import ContactComponent from './components/pages/contact/ContactComponent'
 
 Vue.config.productionTip = false
-Vue.use(Vuerouter)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', name: 'post list', component: PostListComponent },
+  { path: '/post', name: 'post', component: PostComponent },
+  { path: '/contact', name: 'contact', component: ContactComponent }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
