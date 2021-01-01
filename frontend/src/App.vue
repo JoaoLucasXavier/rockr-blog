e341d810961e456b650a51b263f82dd27df588f2
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-01-01 16:15:18 -0300

    Add post list page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <section class="main">
+    <navbar></navbar>
+    <section class="content">
+      <router-view></router-view>
+    </section>
+  </section>
 </template>
 
 <script>
 
+import navbar from './components/nav/NavBarComponent'
+
 export default {
-  name: 'App',
-  components: {}
+  name: 'main',
+  components: {
+    navbar
+  }
 }
 </script>
 
 <style>
-#app {
+
+* {
   margin: 0;
   padding: 0;
+}
+
+body {
+  background: linear-gradient(90deg, #f1a10a 0%, #342303 100%);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.main {
+  display: grid;
+  grid-template-areas: "nav" "content";
+  grid-template-rows: 100px 100%;
+  font-family: "Rubik", sans-serif;
+}
+
+.content {
+  grid-area: content;
 }
 </style>
--- frontend/src/components/nav/NavBarComponent.vue
+++ frontend/src/components/nav/NavBarComponent.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Nav bar</h1>
+  <nav class="nav">
+    <header>
+      <span><a href="">Rockr Blog</a></span>
+    </header>
+    <article>
+      <a class="item" href="">Post</a>
+      <button id="contact-btn" class="modal-btn">Contact</button>
+    </article>
+  </nav>
 </template>
 
 <script>
@@ -8,5 +16,46 @@ export default {}
 </script>
 
 <style scoped>
+
+.nav {
+  grid-area: nav;
+  background: #2d2d2d;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.nav > header > span > a {
+  text-decoration: none;
+  color: #fff;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 43px;
+}
+
+.nav > article .item {
+  color: #fff;
+  text-decoration: none;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 38px;
+  padding: 0px 50px;
+}
+
+.modal-btn {
+  color: #fff;
+  text-decoration: none;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 38px;
+  padding: 0px 50px;
+  background-color: #2d2d2d;
+  border:none;
+  cursor: pointer;
+  outline: none;
+}
 
 </style>
