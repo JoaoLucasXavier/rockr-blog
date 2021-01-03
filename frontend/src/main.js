a2dfc0fafecc95c545e8064bc1853edd0f09d8f6
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-01-03 11:15:51 -0300

    Add Sass and Bootstrap libraries

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import PostComponent from './components/pages/post/PostComponent'
 import PostListComponent from './components/pages/post-list/PostListComponent'
 
@@ -9,7 +11,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'post list', component: PostListComponent },
+  { path: '/', name: 'post-list', component: PostListComponent },
   { path: '/post', name: 'post', component: PostComponent },
 ]
 
