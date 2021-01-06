f2f167f0a76a6ddbfdf1aa4b0493ced1ccec2db1
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-01-06 20:17:03 -0300

    Creating controllers and routes - API

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+# Posts
+Route::get('/post/index', [PostController::class, 'index'])->name('List all posts');
+Route::get('/post/show/{id}', [PostController::class, 'show'])->name('List post by id');
+
+# Contacts
+Route::post('/contact/store', [ContactController::class, 'store'])->name('New contact');
